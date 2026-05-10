8da93db64ea99de2294aefbc71cde3bfca8b3abf
i4vnmacha2010-droid <i4vnmacha2010@gmail.com>
2026-05-10 13:45:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# Import necessary libraries
+from tkinter import *
+# Create window
+window = Tk()
+
+# Set window Title and Geometry
+window.title("Demo Window")
+window.geometry("400x300")
+
+# Start the GUI event loop
+window.mainloop()
